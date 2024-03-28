27de6a2190c461eba95cfca22ecdadfdc5ec897b
Umer Ahmed <umerahmed996@gmail.com>
2024-03-28 08:41:09 +0500

    first commit

---
+++ app/src/main/java/com/fyp/mandiapp/activities/PaymentActivity.java
@@ -0,0 +1,31 @@
+package com.fyp.mandiapp.activities;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.fyp.mandiapp.databinding.ActivityPaymentBinding;
+import com.fyp.mandiapp.utils.Constants;
+
+public class PaymentActivity extends AppCompatActivity {
+
+    ActivityPaymentBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityPaymentBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        String orderCode = getIntent().getStringExtra("orderCode");
+
+        binding.webview.setMixedContentAllowed(true);
+        binding.webview.loadUrl(Constants.PAYMENT_URL + orderCode);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        finish();
+        return super.onSupportNavigateUp();
+    }
+}
